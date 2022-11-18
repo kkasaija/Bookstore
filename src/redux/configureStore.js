c5aca66c1c60bad188d9641c6a325655397dd533
Kasaija Kenneth <kasaijak@gmail.com>
2022-11-18 10:56:44 +0300

    Modify store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,13 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { combineReducers, configureStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const rootReducer = combineReducers({ books: booksReducer, categories: categoriesReducer });
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const rootReducer = combineReducers({
+  books: booksReducer,
+  categories: categoriesReducer,
+});
+
+const store = configureStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
